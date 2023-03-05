341b31f960547a1665a113b9542472ba2cc4bfec
Alena Deviatova <aedeviatova@yandex.ru>
2023-03-05 12:32:13 +0300

    feat: add base structure and functionality to the project

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,84 @@
+<script setup>
+import {ref} from "vue";
+
+const showModal = ref(false)
+const newThought = ref('')
+const thoughts = ref([])
+const errorMessage = ref('')
+
+function getRandomColor() {
+  return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+}
+const addThought = () => {
+  if (newThought.value.length > 5) {
+    thoughts.value.push({
+      id: Math.floor(Math.random() * 1000000),
+      text: newThought.value,
+      date: new Date(),
+      bgColor: getRandomColor(),
+    })
+    showModal.value = false
+    newThought.value = ''
+    errorMessage.value = ''
+  }
+  return errorMessage.value = 'Try extending your thought to 5 words or more'
+}
+
+</script>
 <template>
   <v-app>
     <v-main>
-      <router-view/>
+      <div class="container mx-auto pa-6">
+        <header class="mb-10 d-flex justify-space-between">
+          <h1 class="text-h3">Thoughts</h1>
+          <v-dialog
+              v-model.trim="showModal"
+              width="auto"
+          >
+            <template v-slot:activator="{ props }">
+              <v-btn
+                  color="primary"
+                  icon="mdi-plus"
+                  v-bind="props"
+                  rounded
+              >
+              </v-btn>
+            </template>
+
+            <v-card class="bg-lime-lighten-5" :style="{minWidth: '450px'}">
+              <v-card-item class="pt-8 pb-8">
+                <p v-if="errorMessage" class="text-red-lighten-1 text-body-2">{{errorMessage}}</p>
+                <v-textarea
+                    v-model="newThought"
+                    auto-grow
+                    variant="solo"
+                    bg-color="secondary"
+                    placeholder="Your thought..."
+                    autofocus/>
+                <v-btn @click="addThought" variant="tonal" color="primary" class="mb-3" block>Add Thought</v-btn>
+                <v-btn variant="tonal" color="primary" class="" block @click="showModal = false">Close</v-btn>
+              </v-card-item>
+            </v-card>
+          </v-dialog>
+        </header>
+        <v-main>
+          <v-card v-for="thought in thoughts"
+                  :key="thought.id" :style="{maxWidth: '250px', backgroundColor: thought.bgColor}"
+                  class="d-flex flex-column">
+            <v-icon icon="mdi-close-circle-outline" size="large" class="align-self-end pt-2 pr-2" :style="{cursor: 'pointer'}"></v-icon>
+            <v-card-text class="text-body-1">{{thought.text}}</v-card-text>
+            <v-card-text class="text-body-2 font-weight-bold">{{thought.date.toLocaleDateString("en-US")}}</v-card-text>
+          </v-card>
+        </v-main>
+      </div>
     </v-main>
   </v-app>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<style scoped>
 
-export default defineComponent({
-  name: 'App',
+.container {
+  max-width: 1000px;
+}
 
-  data () {
-    return {
-      //
-    }
-  },
-})
-</script>
+</style>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,6 +5,31 @@ import 'vuetify/styles'
 // Vuetify
 import { createVuetify } from 'vuetify'
 
+const tenderlyTheme = {
+    dark: false,
+    colors: {
+        background: '#E0F2F1',
+        surface: '#D4E157',
+        primary: '#00695C',
+        'primary-darken-1': '#00796B',
+        secondary: '#DCE775',
+        'secondary-darken-1': '#AFB42B',
+        'accentColor': '#EEFF41',
+        error: '#FF8A65',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+    }
+}
+
 export default createVuetify(
+    {
+        theme: {
+            defaultTheme: 'tenderlyTheme',
+            themes: {
+                tenderlyTheme,
+            }
+        }
+    }
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 )
